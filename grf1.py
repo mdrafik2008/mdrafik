ecfd33622dbd37a7566692e890efacb27c20c598
mdrafik2008 <42990373+mdrafik2008@users.noreply.github.com>
2018-10-31 10:10:18 +0530

    Add files via upload

---
+++ grf1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+
+left=[1,2,3,4,5]
+
+height=[10,24,26,28,32]
+
+tick_label=['one','two','three','four','five']
+
+plt.bar(left,height, tick_label=tick_label,
+	width=0.8,color=['red','green'])
+
+plt.xlabel('x - axis')
+plt.ylabel('y - axis')
+plt.title('my bar chart')
+plt.show()	
